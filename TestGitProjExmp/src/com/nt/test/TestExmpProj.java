313662f4d2513850e1a1016f4efe56c5419b1d3f
welcome <welcome@DESKTOP-I7KIDH0>
2018-08-27 00:14:34 +0530

    add some sys stmts

--- TestGitProjExmp/src/com/nt/test/TestExmpProj.java
+++ TestGitProjExmp/src/com/nt/test/TestExmpProj.java
@@ -4,6 +4,9 @@ public class TestExmpProj {
 
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		System.out.println("hello mallikarjun");
+		System.out.println("hello java");
+		System.out.println("hello spring");
 
 	}
 
