c7c02e9edaa254d0f83a3da66077545cacde9707
welcome <welcome@DESKTOP-I7KIDH0>
2018-08-27 00:26:33 +0530

    one stmt is deleted

--- TestGitProjExmp/src/com/nt/test/TestExmpProj.java
+++ TestGitProjExmp/src/com/nt/test/TestExmpProj.java
@@ -4,7 +4,6 @@ public class TestExmpProj {
 
 	public static void main(String[] args) {
 		System.out.println("hello world");
-		System.out.println("hello mallikarjun");
 		System.out.println("hello java");
 		System.out.println("hello spring");
 
